b5654966e6974f95b54de1fcbabd5660910bfc17
unknown <david.jung@uniat.edu.mx>
2025-06-02 13:30:46 -0600

    Reworked movement and collisions

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -13,6 +13,10 @@ public class PlayerMovement : MonoBehaviour
     public WeaponHolder weaponHolder;
     public GameObject rangedWeaponPrefab;
     public GameObject meleeWeaponPrefab;
+    Vector2 movementCollision= new Vector2(1,1);
+    float speed = .0625f;
+    public LayerMask Walls;
+    
 
     private void Awake()
     {
@@ -47,7 +51,15 @@ public class PlayerMovement : MonoBehaviour
     public void MovePlayer(InputAction.CallbackContext ctx)
     {
         Vector2 direction = ctx.ReadValue<Vector2>();
-        transform.Translate(new Vector2(3.5f,3.5f)*direction*Time.deltaTime);
+        direction=new Vector2(Mathf.Clamp(direction.x, -100, 100), Mathf.Clamp(direction.y, -100, 100));
+        Debug.Log(direction);
+        if (Physics2D.Raycast(transform.position, Vector2.down, .23f, Walls)) { direction.y = Mathf.Clamp(direction.y, 0, 1); }
+        if (Physics2D.Raycast(transform.position, Vector2.left, .23f, Walls)) { direction.x = Mathf.Clamp(direction.x, 0, 1); }
+        if (Physics2D.Raycast(transform.position, Vector2.right, .23f, Walls)) { direction.x = Mathf.Clamp(direction.x, -1, 0); }
+        if (Physics2D.Raycast(transform.position, Vector2.up, .23f, Walls)) { direction.y = Mathf.Clamp(direction.y, -1, 0); }
+     
+        transform.Translate(speed*direction*Time.deltaTime);
+        //playerRB.AddForce(direction*speed*Time.fixedDeltaTime);
     }
 
     //InputActions
